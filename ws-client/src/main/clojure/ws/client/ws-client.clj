98dfc0428b19ab8d45c71a35bf0d64798901d851
Ben Christensen <benjchristensen@netflix.com>
2013-01-21 09:57:46 -0800

    first version of ws-client
    Written in Clojure.
    Sends requests against given server with defined number of threads (or defaults to number of processors)
    
          Example: (run-load-test "http://localhost:8888/ws-java-servlet-blocking/testA" 2)
    
    Currently does not have a run script, only executable through Clojure REPL. A script or gradle task will come.
    
    It outputs logs like this for now:
    
    timestamp:1358790809982 threads: 2 duration: 362.656
    timestamp:1358790810135 threads: 2 duration: 333.307
    timestamp:1358790810316 threads: 2 duration: 333.771
    timestamp:1358790810476 threads: 2 duration: 340.92
    timestamp:1358790810659 threads: 2 duration: 342.781
    timestamp:1358790810810 threads: 2 duration: 333.922
    timestamp:1358790810941 threads: 2 duration: 281.883
    
    This will evolve as I work on the tools to convert from logs across distributed clients into a single report with graphs.

---
+++ ws-client/src/main/clojure/ws/client/ws-client.clj
@@ -0,0 +1,71 @@
+(ns ws.client.ws-client
+  (:gen-class)
+  (:require [clj-http.client :as http]
+            [clojure.java.io :as io]))
+
+(declare send-requests response-log)
+
+(defn run-load-test
+  "Execute load test against given URL
+
+  Examples:
+    (run-load-test \"http://localhost:8888/ws-java-servlet-blocking/testA\") ; to use the number of CPUs for numThreads
+    (run-load-test \"http://localhost:8888/ws-java-servlet-blocking/testA\" 2) ; to run with 2 threads
+  "
+  ([url] (run-load-test url (-> (Runtime/getRuntime) .availableProcessors)))
+  ([url num-threads]
+    (println "Starting load test")
+    (with-open [log-writer (writer "/tmp/test2.txt")]
+      (println "Log file opened")
+      (http/with-connection-pool {:timeout 500 :threads 20 :insecure? false :default-per-route 20}
+        (let [futures (doall (for [i (range num-threads)]
+                             (future
+                               (println "send-requests starting in thread: " (Thread/currentThread))
+                               (send-requests url 10 log-writer num-threads))))]
+          (println "starting")
+          (doseq [f futures]
+            ; wait for each future to complete
+            (deref f))
+          ; (println (deref f 900 #(future-cancel f))) ; this isn't working (returns function but doesn't execute it)
+          (println "Done load test."))))))
+  
+
+; used for sending response logs to and serializing them to output
+(def response-log (agent 0))
+
+(defmacro duration-in-millis
+  "Evaluates expr and records the time it took in millis.
+   Passes the time as the first argument to 'on-complete' then returns the expression response."
+  [expr on-complete]
+  `(let [start# (. System (nanoTime))
+         ret# ~expr]
+     (~on-complete (/ (double (- (. System (nanoTime)) start#)) 1000000.0))
+     ret#))
+
+(defn- send-response-log
+  "Function that will 'send-off' for asynchronously logging response metrics.
+   This function itself should not nothing blocking."
+  [time-in-millis log-writer num-threads]
+  (send-off response-log (fn [count]
+              (.write log-writer (str "timestamp:" (System/currentTimeMillis) " threads: " num-threads " duration: " time-in-millis "\n"))
+              (if (= 10 count) 
+                (do 
+                  (.flush log-writer); flush writer every 10 log entries
+                  0) ; return 0 as the value to reset count
+                (inc count) ; increment the agent value as a counter
+              )
+              )))
+
+(defn- send-requests
+  "Send an HTTP request 'count' times and log metrics after each response."
+  [url count log-writer num-threads]
+  (dotimes [n count] 
+      (let
+        ; we generate a random id to seed the request with a different arg each request
+        [url (str url "?id=" (rand-int 999999))]
+        ; perform http/get inside duration-in-millis macro which passes the
+        ; duration of the http/get function to the following function 
+        (duration-in-millis (http/get url) #(send-response-log % log-writer num-threads))
+        )))
+
+
