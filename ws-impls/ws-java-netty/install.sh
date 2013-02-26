6b77bfdf86ed18e973157336339acda911cd8fbd
Nitesh Kant <nitesh1706@gmail.com>
2013-02-26 11:27:10 -0800

    Update to install/startup scripts

--- ws-impls/ws-java-netty/install.sh
+++ ws-impls/ws-java-netty/install.sh
@@ -35,39 +35,28 @@ if [ -z "$backend" ]; then
 	exit
 fi
 
-echo "Install to host: $hostname"
+echo "Installing to host: $hostname"
 echo "SSH command: $sshCommand"
 
 if $update ; then
 	echo "--- Kill all java processes"
 	eval "$sshCommand $hostname 'sudo killall java'"
+	echo "--- Remove previously installed ws-java-netty.zip"
+	eval "$sshCommand $hostname '/bin/rm -R ws-java-netty*'"
 	echo "--- Update from Git"
-	eval "$sshCommand $hostname 'cd WSPerfLab/; git pull'"
-	echo "--- Remove previously installed ws-java-servlet-blocking.war"
-	eval "$sshCommand $hostname '/bin/rm -R apache-tomcat-7.0.37/webapps/ws*'"
+	eval "$sshCommand $hostname 'cd ~/WSPerfLab/; git pull'"
 else
 	echo "--- Shutdown Netflix Tomcat"
 	eval "$sshCommand $hostname 'sudo /etc/init.d/nflx-tomcat stop'" # need to use a different AMI so this can be removed
 	echo "--- Kill all java processes"
 	eval "$sshCommand $hostname 'sudo killall java'"
 	echo "--- Git clone WSPerfLab"
-	eval "$sshCommand $hostname 'git clone git://github.com/benjchristensen/WSPerfLab.git'"
-	echo "--- Download Tomcat 7"
-	eval "$sshCommand $hostname 'wget http://mirrors.gigenet.com/apache/tomcat/tomcat-7/v7.0.37/bin/apache-tomcat-7.0.37.tar.gz'"
-	echo "--- Extract Tomcat 7"
-	eval "$sshCommand $hostname 'tar xzvf apache-tomcat-7.0.37.tar.gz'"
-	echo "--- Delete demo apps from Tomcat 7"
-	eval "$sshCommand $hostname '/bin/rm -R apache-tomcat-7.0.37/webapps/docs/ apache-tomcat-7.0.37/webapps/examples/ apache-tomcat-7.0.37/webapps/host-manager/ apache-tomcat-7.0.37/webapps/manager/'"
-	echo "--- Add perf.test.backend.hostname property to catalina.sh"
-	eval "$sshCommand $hostname 'cd apache-tomcat-7.0.37/bin; echo \"99a100,101\" >> catalina.patch'"
-	eval "$sshCommand $hostname 'cd apache-tomcat-7.0.37/bin; echo \"> JAVA_OPTS=\\\"\\\$JAVA_OPTS -Dperf.test.backend.hostname=$backend/ws-backend-mock\\\"\" >> catalina.patch'"
-	eval "$sshCommand $hostname 'cd apache-tomcat-7.0.37/bin; echo \"> \" >> catalina.patch'"
-	eval "$sshCommand $hostname 'cd apache-tomcat-7.0.37/bin; patch catalina.sh catalina.patch'"
+	eval "$sshCommand $hostname 'git clone git://github.com/NiteshKant/WSPerfLab.git'"
 fi
 
-echo "--- Build WSPerfLab"
-eval "$sshCommand $hostname 'cd WSPerfLab/; ./gradlew clean build'"
-echo "--- Copy ws-java-servlet-blocking.war to Tomcat 7"
-eval "$sshCommand $hostname 'cp WSPerfLab/ws-impls/ws-java-servlet-blocking/build/libs/ws-java-servlet-blocking-*-SNAPSHOT.war apache-tomcat-7.0.37/webapps/ws-java-servlet-blocking.war'"
-echo "--- Start Tomcat 7"
-eval "$sshCommand $hostname 'cd apache-tomcat-7.0.37/bin/; ./startup.sh'"
+echo "--- Build ws-java-netty"
+eval "$sshCommand $hostname 'cd WSPerfLab/ws-impls/ws-java-netty/; ./gradlew clean build distZip'"
+echo "--- Copy distribution"
+eval "$sshCommand $hostname 'cp WSPerfLab/ws-impls/ws-java-netty/build/distributions/ws-java-netty-*-SNAPSHOT.zip ~/; cd ~; tar -xvzf ws-java-netty-*-SNAPSHOT.zip'"
+echo "--- Start Netty impl"
+eval "$sshCommand $hostname 'cd ws-java-netty/bin/; nohup ./startWithLog.sh > /dev/null 2>&1 &'"
