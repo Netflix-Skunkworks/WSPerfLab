74faa53a91c27b3a0699e748a1d2f706997cf2c8
Nitesh Kant <nitesh1706@gmail.com>
2013-06-10 16:37:47 -0700

    Added shell scripts for the client

---
+++ ws-client/src/dist/bin/java-client.sh
@@ -0,0 +1,56 @@
+#! /bin/bash +x
+
+#############################################################
+################# ws-client options #####################
+
+WS_CLIENT_OPTS=""
+export WS_CLIENT_OPTS=${WS_CLIENT_OPTS}
+#############################################################
+
+SCRIPT_DIR=$(dirname $BASH_SOURCE)
+SCRIPTNAME=`basename $0`
+ACTUAL_SCRIPT_NAME='ws-client'
+
+while getopts "u:c:r:" opt; do
+  case $opt in
+    u)
+	  uri=$OPTARG
+      ;;
+    c)
+      concurrent_clients=$OPTARG
+      ;;
+    r)
+      requests=$OPTARG
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+if [ -z "$uri" ]; then
+	echo $'\a'-u required for test case uri
+	echo "$0 -u [test uri] -c [Number of concurrent clients] -r (Total expected requests)"
+	exit
+fi
+
+if [ -z "$concurrent_clients" ]; then
+	echo $'\a'-c required for concurrent clients
+	echo "$0 -u [test uri] -c [Number of concurrent clients] -r (Total expected requests)"
+	exit
+fi
+
+if [ -z "$requests" ]; then
+	echo $'\a'-r required for number of requests
+	echo "$0 -u [test uri] -c [Number of concurrent clients] -r (Total expected requests)"
+	exit
+fi
+
+. $SCRIPT_DIR/$ACTUAL_SCRIPT_NAME $uri $concurrent_clients $requests
+
+LOG_DIR=$SCRIPT_DIR/../logs/
+ERR_LOG_FILE_NAME="ws-java-client-err.log"
+
+if [ -s $LOG_DIR/$LOG_FILE_NAME ]; then
+    echo "There are some errors in the error log file $ERR_LOG_FILE_NAME"
+fi
