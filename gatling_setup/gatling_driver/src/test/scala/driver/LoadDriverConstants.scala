13e878eb86cce3a89449846e61201e4e849ba798
Seth Katz <katzseth22202@gmail.com>
2013-10-13 22:31:04 -0700

    add id randomly to the URl

--- gatling_setup/gatling_driver/src/test/scala/driver/LoadDriverConstants.scala
+++ gatling_setup/gatling_driver/src/test/scala/driver/LoadDriverConstants.scala
@@ -1,11 +1,20 @@
 package driver
 
 object LoadDriverConstants {
+    import java.lang.ThreadLocal
+    import java.util.Random
+
     val repetitions = 250
     val warmup = 5
     val totalUsers = 8000
     val rampTime = totalUsers/1000+1
     var file = "/tmp/loadtesturl.txt"
+
+    val randomPerThread : ThreadLocal[Random] = {
+        new ThreadLocal[Random] {
+            override protected def initialValue() : Random =  { new Random }
+        }
+    }
 
     /** grab the host name from /tmp/servers.txt (from grab_ids.py) - since there should only be 1 ws_impl, just grab the first host */
     val url : String = {
@@ -14,5 +23,10 @@ object LoadDriverConstants {
         val line = source.getLines.find(_ => true).getOrElse("NO HOST SPECIFIED")
         source.close
         line
+    }
+
+    def nextURL() : String = {
+        val id = randomPerThread.get.nextInt(90000)+10000 // pattern from validate.py script [10000, 100000) range
+        return url + "testA?id=" + id
     }
 }
