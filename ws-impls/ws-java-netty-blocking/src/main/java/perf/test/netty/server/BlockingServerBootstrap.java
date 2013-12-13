1a3665194bc794cce4540b2cbe3c43be9452dd7d
Nitesh Kant <nkant@netflix.com>
2013-12-12 16:05:22 -0800

    Adding event loop counts for blocking netty impl

--- ws-impls/ws-java-netty-blocking/src/main/java/perf/test/netty/server/BlockingServerBootstrap.java
+++ ws-impls/ws-java-netty-blocking/src/main/java/perf/test/netty/server/BlockingServerBootstrap.java
@@ -9,7 +9,7 @@ public class BlockingServerBootstrap extends ServerBootstrap {
 
     static {
         System.setProperty(PropertyNames.ClientIOBlocking.getPropertyName(), "true");
-        System.setProperty(PropertyNames.ClientIOBlocking.getPropertyName(), "true");
+        System.setProperty(PropertyNames.ServerIOBlocking.getPropertyName(), "true");
         System.setProperty(PropertyNames.ServerContextPath.getPropertyName(), "/ws-java-netty-blocking/");
     }
 }
