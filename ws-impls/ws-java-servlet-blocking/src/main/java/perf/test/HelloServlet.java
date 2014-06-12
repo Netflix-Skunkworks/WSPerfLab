073e4b6e370a221294d14beaf4cdee438ae1f441
mhawthorne <mhawthorne@gmail.com>
2014-06-12 15:39:46 -0700

    adding /hello endpoint to tomcat test

---
+++ ws-impls/ws-java-servlet-blocking/src/main/java/perf/test/HelloServlet.java
@@ -0,0 +1,19 @@
+package perf.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author mhawthorne
+ */
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Hello world!");
+    }
+
+}
